7e6c33b5a2e755a79b46c93d9ec33d6fa8b6069c
frankleveque <leveque.frank@gmail.com>
2017-06-09 22:25:20 -0700

    add tests

--- tests/btnode_test.cpp
+++ tests/btnode_test.cpp
@@ -7,11 +7,35 @@
 
 TEST_CASE("Binary Tree class tests"){
     {
-    BTNode<int> root;
-    REQUIRE(*root.value == 0);
+        BTNode<int> root(0);
+        REQUIRE(root.value == 0);
     }
     {
-    BTNode<std::string> root;
-    REQUIRE(*root.value == "");
-    }   
+        BTNode<int> root;
+        REQUIRE(root.value == 0);
+    }
+    {
+        BTNode<std::string> root;
+        REQUIRE(root.value == "");
+    }
+    {
+        BTNode<uint8_t> root(5);
+        for(auto i=0; i<10; ++i){
+            root.insert(BTNode<uint8_t>(i));
+        }
+
+        REQUIRE(root.size() == 10);
+
+    }
+    {
+        BTNode<uint32_t> root(1000);
+        for(auto i=0; i<2000; ++i){
+            root.insert(BTNode<uint32_t>(i));
+        }
+        for(auto i=1999; i>=0; --i){
+            root.insert(BTNode<uint32_t>(i));
+        }
+        REQUIRE(root.size() == 2000);
+    }
+
 }
